81ba8d6da08fa58c25d102c1b1d38ca822dc994a
Jerry Spice <36059502+Jerry-Spice@users.noreply.github.com>
2021-10-06 21:22:03 -0400

    reworked direction math functions

--- AntClass.py
+++ AntClass.py
@@ -3,49 +3,36 @@ import pygame, random, math
 class Ant(object):
     def __init__(self):
         self.x, self.y = 300,300
-        self.wanderAmount = 0.1
+        self.wanderAmount = 5
         # self.direction = [0,0]
         self.color = (255,255,255)
         self.body = pygame.Surface((10,10)) #10x10 pixel square
         self.body.fill(self.color)
         self.id = 0
-        self.speed = 1
-        self.angle = 0
-        self.direction = [math.cos(self.angle)*self.speed, math.sin(self.angle)*self.speed]
+        self.speed = 5
+        self.angle = 180
+        self.direction = [math.cos(self.angle*(math.pi/180)) / self.speed, math.sin(self.angle*(math.pi/180)) / self.speed]
+        
         self.targetAngle = None
 
         self.foodId = None
-        # f = open("C:/Users/joshl/OneDrive/Desktop/pygame/AntsCrawling/AntData/Ant#"+str(self.id)+".txt","w+")
-        # f.close()
-        self.log = ""
         # self.body.convert()
     
-    def logActions(self, message):
-        f = open("C:/Users/joshl/OneDrive/Desktop/pygame/AntsCrawling/AntData/Ant#"+str(self.id)+".txt","a+")
-        f.write(message+"\n")
-        f.close()
-
     def move(self, spaceToExploreLimits):
-        self.direction = [math.cos(self.angle)*self.speed, math.sin(self.angle)*self.speed]
-        # print(self.angle)
+        self.direction = [math.cos(self.angle*(math.pi/180)) / self.speed, math.sin(self.angle*(math.pi/180)) / self.speed]
+        # print(self.direction)
+        print(self.angle)
         if self.x + 10 + self.direction[0] < spaceToExploreLimits[0] and self.x + self.direction[0] > 0:
-            self.x += self.direction[0] * 0.5
-            self.log += "X -> "+str(self.x)
+            self.x += self.direction[0]
         else:
             self.direction[0] *= -1
-            self.log += "X -> "+str(self.x)
         if self.y + 10 + self.direction[1] < spaceToExploreLimits[1] and self.y + self.direction[1] > 0:
-            self.y += self.direction[1] * 0.5
-            self.log += "X -> "+str(self.x)
+            self.y += self.direction[1]
         else:
             self.direction[1] *= -1
-            self.log += "X -> "+str(self.x)
+    
+
     def wander(self, spaceToExploreLimits, availableFood):
-        # while self.angle > 360:
-        #     self.angle = self.angle-360
-        # while self.angle < 0:
-        #     self.angle = self.angle+360
-        # self.findFood(availableFood)
         if self.targetAngle == None:
             if random.randint(0,1) == 0:
                 self.angle += self.wanderAmount
@@ -54,26 +41,21 @@ class Ant(object):
             self.move(spaceToExploreLimits)
         else:
             self.angle = self.targetAngle
-            # self.direction = [round(math.cos(self.angle)*self.speed), round(math.sin(self.angle)*self.speed)]
-            print(str(round(self.angle))+":"+str(round(self.targetAngle)))
             self.move(spaceToExploreLimits)
     
     def display(self, screen):
         screen.blit(self.body, (self.x, self.y))
         
-    def findFood(self, foodArray):
-        closestDistance = 10000000
-        for i in range(len(foodArray)):
-            distanceToFood = math.sqrt((foodArray[i].y - self.y)**2 + (foodArray[i].x - self.x)**2)
-            if distanceToFood < closestDistance:
-                closestDistance = distanceToFood
-        if closestDistance != 10000000 and (distanceToFood <= 100 and distanceToFood != 0):
-            changeInY = (foodArray[i].y - self.y)
-            self.targetAngle = math.asin((changeInY/distanceToFood)) + self.angle
-            self.foodId = foodArray[i].id
-        elif distanceToFood == 0:
-            pass
-        else:
-            self.targetAngle = None
+    def findFood(self, availableFood):
+        closestDistance = 10000000000
+        closestId = -1
+        for food in availableFood:
+            closestDistance = math.sqrt((self.x - food.x)**2 + (self.y - food.y)**2) if math.sqrt((self.x - food.x)**2 + (self.y - food.y)**2) < closestDistance else closestDistance
+            closestId = food.id if closestDistance == math.sqrt((self.x - food.x)**2 + (self.y - food.y)**2) else closestId
+        if closestDistance != 0 or closestDistance != 10000000000 and closestId != -1:
+            """Todo:
+                add find food method by finding the x difference and then the cos (SohCahToa) for the target angle
+            """
+            self.targetAngle = math.cos()
         
 
